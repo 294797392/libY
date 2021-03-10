2b80f0b3ffb61c0ecd72d853caef8b9cdcd45d06
张宇锋 <294797392@qq.com>
2021-03-10 17:00:37 +0800

    .

--- sources/matrix/matrix.c
+++ sources/matrix/matrix.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "matrix/matrix.h"
+
 struct matrix_s
 {
     void *ptr;
 };
+
+void matrix_mul(matrix *a, matrix *b)
+{
+}
---
+++ sources/matrix/matrix.h
@@ -0,0 +1,8 @@
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+typedef struct matrix_s matrix;
+
+void matrix_mul(matrix *a, matrix *b);
+
+#endif
